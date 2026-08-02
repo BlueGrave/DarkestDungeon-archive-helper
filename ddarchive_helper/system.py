667865aa0dfa23ad459b271c2a188348d2e603a8
BlueGrave <53641109+BlueGrave@users.noreply.github.com>
2026-08-02 18:51:24 +0800

    Update system.py

--- ddarchive_helper/system.py
+++ ddarchive_helper/system.py
@@ -5,11 +5,19 @@ import subprocess
 
 def is_darkest_running() -> bool:
     try:
+        # 配置 Windows 下隐藏子进程黑窗口
+        startupinfo = None
+        if sys.platform == "win32":
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+            startupinfo.wShowWindow = subprocess.SW_HIDE
+            
         result = subprocess.run(
             ["tasklist", "/FI", "IMAGENAME eq Darkest.exe"],
             capture_output=True,
             text=True,
             check=False,
+            startupinfo=startupinfo, # <-- 传入隐藏参数
         )
         output = result.stdout.lower()
         return "darkest.exe" in output
